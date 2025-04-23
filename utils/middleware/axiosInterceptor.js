73c9f46a5b605bdc3db1cdde97941c03e927b7f5
Mahmoud Maher <mahmoudmaherofficial@gmail.com>
2025-04-23 05:38:26 +0200

    fix: axios url

--- utils/middleware/axiosInterceptor.js
+++ utils/middleware/axiosInterceptor.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
 });
 
